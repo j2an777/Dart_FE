5a4ac541ea16afd6283479e766077d50efcf5bc0
soohyun <gamjatan9@gmail.com>
2024-05-29 09:54:10 +0900

    Feat: 전시 생성 페이지 추가

---
+++ src/pages/post/styles.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  margin: 0 80px;
+  height: 100vh;
+  outline: 2px solid black;
+`;
+
+export const Quit = styled.div`
+  display: flex;
+  justify-content: end;
+  border-bottom: 2px solid black;
+`;
