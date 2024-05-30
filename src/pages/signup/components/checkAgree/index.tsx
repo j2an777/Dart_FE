877cc84b87a9a758409a10451daf7ac23b6e6b67
kangsinbeom <kanian16@naver.com>
2024-05-31 08:39:50 +0900

    Feat: 약관동의 만들기
    ui 및 기능 만들기

---
+++ src/pages/signup/components/checkAgree/index.tsx
@@ -0,0 +1,55 @@
+import { Button, Icon, Text } from '@/components';
+import { alertStore } from '@/stores/alert';
+
+import * as S from './styles';
+
+interface CheckAgreeProps {
+  title: string;
+  isTotal?: boolean;
+  isLast?: boolean;
+  checked?: boolean;
+  onClick?: (value: string) => void;
+  value: string;
+}
+
+const CheckAgree = ({
+  title,
+  isTotal = false,
+  isLast = false,
+  checked = false,
+  onClick = () => {},
+  value,
+}: CheckAgreeProps) => {
+  const open = alertStore((state) => state.open);
+  return (
+    <S.Container isLast={isLast} isTotal={isTotal}>
+      <Icon
+        value="check"
+        color={checked ? 'white' : 'gray400'}
+        onClick={() => onClick(value)}
+      />
+      <Text bold="regular" color="white" typography="t4">
+        {title}
+      </Text>
+      {!isTotal && (
+        <Button
+          buttonType="reverseRectangleWhite"
+          size="xs"
+          type="button"
+          onClick={() =>
+            open({
+              title,
+              buttonLabel: '내용 접기',
+              description: '안녕',
+              onClickButton: () => {},
+            })
+          }
+        >
+          이용약관
+        </Button>
+      )}
+    </S.Container>
+  );
+};
+
+export default CheckAgree;
--- src/pages/signup/components/index.ts
+++ src/pages/signup/components/index.ts
@@ -1,6 +1,9 @@
-export { default as TitleNumber } from './titleNumber';
-export { default as SignupForm } from './signupForm';
-export { default as SignupButtons } from './signupButtons';
 export { default as InputArea } from './inputArea';
-export { default as SignupFormLayout } from './signupFormLayout';
+export { default as CheckAgree } from './checkAgree';
+export { default as SignupAgree } from './signupAgree';
+export { default as TitleNumber } from './titleNumber';
+export { default as SignupButtons } from './signupButtons';
 export { default as SignupTextarea } from './signupTextarea';
+export { default as SignupFormLayout } from './signupFormLayout';
+export { default as SignupOptionalForm } from './signupOptionalForm';
+export { default as SignupEssentailForm } from './signupEssentailForm';
