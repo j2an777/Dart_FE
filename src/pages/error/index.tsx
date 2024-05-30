71358d8990704397ee38567559fb976095bee3e0
j2an777 <ha991104@gmail.com>
2024-05-30 10:11:02 +0900

    feat: 404페이지 파일 생성
    404 페이지 생성 위한 파일 생성

---
+++ src/pages/error/index.tsx
@@ -0,0 +1,20 @@
+
+import * as S from './styles';
+
+const ErrorPage = () => {
+  return (
+    <S.Container>
+        <S.Top>
+
+        </S.Top>
+        <S.Middle>
+
+        </S.Middle>
+        <S.Bottom>
+
+        </S.Bottom>
+    </S.Container>
+  )
+}
+
+export default ErrorPage
