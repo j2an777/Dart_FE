170003c76b1942b595c543e8f73ddc58d4a5bee9
soohyun <gamjatan9@gmail.com>
2024-06-03 10:41:22 +0900

    Feat: react-hook-form 적용
    react-hook-form을 통한 form 데이터 관리

--- src/pages/post/styles.ts
+++ src/pages/post/styles.ts
@@ -1,4 +1,7 @@
+import { buttonSizeMap, buttonTypeMap } from '@/styles/button';
+import { colors } from '@/styles/colorPalette';
 import { LayoutMap } from '@/styles/layout';
+import { typographyMap } from '@/styles/typography';
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
@@ -6,9 +9,10 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: start;
+  margin-bottom: 50px;
 `;
 
-export const Block = styled.div`
+export const Box = styled.div`
   max-width: 1440px;
   outline: 2px solid black;
 `;
@@ -18,4 +22,37 @@ export const Quit = styled.div`
   justify-content: end;
   padding: 5px 10px;
   border-bottom: 2px solid black;
+`;
+
+export const Block = styled.div`
+  ${LayoutMap.displayFlex}
+  justify-content: center;
+  height: 80px;
+  border-top: solid 2px ${colors.black};
+`;
+
+export const Submit = styled.button`
+  ${buttonSizeMap.smMd}
+  ${buttonTypeMap.reverseRadius}
+`;
+
+export const InputBox = styled.input<{ width?: number; height?: number }>`
+  line-height: 1;
+  border: 2px solid ${colors.black};
+  color: ${colors.black};
+  padding: 20px;
+  ${typographyMap.t6};
+  width: ${({ width = '100%' }) => `${width}px`};
+  height: ${({ height = '100%' }) => `${height}px`};
+`;
+
+export const TextBox = styled.textarea<{ width?: number; height?: number }>`
+  line-height: 1;
+  border: 2px solid ${colors.black};
+  color: ${colors.black};
+  padding: 20px;
+  width: ${({ width = '100%' }) => `${width}px`};
+  height: ${({ height = '100%' }) => `${height}px`};
+  resize: none;
+  overflow: auto;
 `;
