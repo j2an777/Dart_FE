4093a92b6038cad095d412857868dacf6423cd88
j2an777 <ha991104@gmail.com>
2024-06-10 23:57:40 +0900

    style: 애니메이션 초안 작성
    framer motion 학습 및 초안 작성

--- src/pages/intro/components/introduce/index.tsx
+++ src/pages/intro/components/introduce/index.tsx
@@ -3,30 +3,51 @@ import * as S from './styles';
 import InItem1 from '@/assets/images/introduceItem1.png';
 import InItem2 from '@/assets/images/introduceItem2.png';
 import InItem3 from '@/assets/images/introduceItem3.png';
+import { motion } from 'framer-motion';
 
 const Introduce = () => {
   return (
     <S.Container>
-        <S.IntroduceBox>
-            <Text typography='t0' color='white' bold='bold' className='title'>
-                D'<span>art</span>
-                <br/>
-                GALLERY
-            </Text>
-            <Text typography='t2' color='white' bold='semibold'>"예술의 공유, 전시의 즐거움"</Text>
-            <S.IntroduceBlock>
-                <Text typography='t2' className='demoTitle' bold='medium'>전시 <span>&nbsp;모두가 즐기는 전시</span></Text>
-                <Text typography='t4' bold='thin' color='gray100'>
-                    사용자 모두 전시를 열 수 있습니다.
+        <motion.div 
+            initial={{ opacity: 0, x: -50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: false }}
+            transition={{
+                ease: 'easeInOut',
+                duration: 0.5,
+                x: { duration: 1}
+            }}>
+            <S.IntroduceBox>
+                <Text typography='t0' color='white' bold='bold' className='title'>
+                    D'<span>art</span>
                     <br/>
-                    자신의 작품을 자유롭게 보여줄
-                    <br/>
-                    기회를 제공합니다.
+                    GALLERY
                 </Text>
-            </S.IntroduceBlock>
-        </S.IntroduceBox>
+                <Text typography='t2' color='white' bold='semibold'>"예술의 공유, 전시의 즐거움"</Text>
+                <S.IntroduceBlock>
+                    <Text typography='t2' className='demoTitle' bold='medium'>전시 <span>&nbsp;모두가 즐기는 전시</span></Text>
+                    <Text typography='t4' bold='thin' color='gray100'>
+                        사용자 모두 전시를 열 수 있습니다.
+                        <br/>
+                        자신의 작품을 자유롭게 보여줄
+                        <br/>
+                        기회를 제공합니다.
+                    </Text>
+                </S.IntroduceBlock>
+            </S.IntroduceBox>
+        </motion.div>
         <S.DemoBox>
-            <img src={InItem1} alt='소개 이미지1' />
+            <motion.div
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: false }}
+                transition={{
+                    ease: 'easeInOut',
+                    duration: 1,
+                    x: { duration: 1}
+                }}>
+                <img src={InItem1} alt='소개 이미지1' />
+            </motion.div>
             <S.DemoBlock>
                 <Text typography='t4' color='white' bold='semibold'>
                     D'<span>art</span>
