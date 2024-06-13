0e6aca910b7cbda7e4795868722cbc24752c9d24
j2an777 <ha991104@gmail.com>
2024-06-13 18:55:17 +0900

    fix: 전시 페이지 UI 수정 및 기능 추가
    데이터 개수에 따른 UI 조절 및 우클릭 방지 추가

--- src/pages/gallery/components/galleryMain/index.tsx
+++ src/pages/gallery/components/galleryMain/index.tsx
@@ -5,6 +5,7 @@ import GalleryDetail from '../galleryDetail';
 import { galleryImages } from '@/types/gallery';
 import { useParams } from 'react-router-dom';
 import { getGallery } from '@/apis/gallery';
+import { Text } from '@/components';
 
 const GalleryMain = () => {
   const [degrees, setDegrees] = useState(0);
@@ -12,6 +13,7 @@ const GalleryMain = () => {
   const [popUp, setPopUp] = useState(false);
   const [selectedData, setSelectedData] = useState<galleryImages | null>(null);
   const [transZ, setTransZ] = useState(400);
+  const [size, setSize] = useState(250);
   const [dataDegree, setDataDegree] = useState(0);
 
   const { galleryId: galleryIdStr } = useParams<{ galleryId?: string }>();
@@ -25,15 +27,35 @@ const GalleryMain = () => {
   useEffect(() => {
     if (galleryData) {
       setDataDegree(360 / galleryData.images.length);
-      if (galleryData.images.length === 9) {
-        setTransZ(450);
-      } else if (galleryData.images.length >= 10) {
-        setTransZ(480);
-      } else {
-        setTransZ(400);
+  
+      switch (true) {
+        case (galleryData.images.length >= 11 && galleryData.images.length < 15):
+          setSize(200);
+          setTransZ(480);
+          break;
+        case (galleryData.images.length >= 15):
+          setSize(150);
+          setTransZ(550);
+          break;
+        default:
+          setTransZ(500);
+          setSize(250);
+          break;
       }
     }
   }, [galleryData]);
+  
+  useEffect(() => {
+    const handleContextMenu = (e: MouseEvent) => {
+      e.preventDefault();
+    };
+    // document 또는 특정 요소에 이벤트 리스너 추가
+    document.addEventListener('contextmenu', handleContextMenu);
+    return () => {
+      // 컴포넌트 언마운트 시 이벤트 리스너 제거
+      document.removeEventListener('contextmenu', handleContextMenu);
+    };
+  }, []);
 
   const onHandleChange = (translate: string) => {
     if (!galleryData) return;
@@ -67,27 +89,30 @@ const GalleryMain = () => {
 
   return (
     <S.Container>
-      <S.MainBlock degrees={degrees}>
-        {galleryData?.images.slice(0, 10).map((gallery: galleryImages, index: number) => (
+      <S.MainBlock degrees={degrees} size={size}>
+        {galleryData?.images.map((gallery: galleryImages, index: number) => (
           <S.ImageBox 
-            key={index} 
+            key={index}
             i={index} 
             isFront={index === frontIndex} 
             dataDegree={dataDegree}
             transZ={transZ}
             onClick={() => onHandlePopup(gallery)}>
             <img src={gallery.image} alt={gallery.imageTitle} />
-            <S.ContentBox>
-              <h1>{gallery.imageTitle}</h1>
-              <p>{gallery.description}</p>
+            <S.ContentBox size={size}>
+              <Text typography='t5' bold='semibold' color='white'>Gallery {index+1}</Text>
+              <Text typography='t7' bold='thin' color='white'>{gallery.imageTitle}</Text>
             </S.ContentBox>
           </S.ImageBox>
         ))}
+        <Text typography='t3' bold='bold' color='white'>{galleryData.title}</Text>
       </S.MainBlock>
-      <S.BtnBlock>
-        <S.Btn className='previous' onClick={() => onHandleChange('previous')}>Previous</S.Btn>
-        <S.Btn className='next' onClick={() => onHandleChange('next')}>Next</S.Btn>
-      </S.BtnBlock>
+      {galleryData.images.length > 1 && (
+        <S.BtnBlock>
+          <S.Btn className='previous' onClick={() => onHandleChange('previous')}>Previous</S.Btn>
+          <S.Btn className='next' onClick={() => onHandleChange('next')}>Next</S.Btn>
+        </S.BtnBlock>
+      )}
       {popUp && (
         <GalleryDetail imageData={selectedData} onClose={onHandleClose}/>
       )}
