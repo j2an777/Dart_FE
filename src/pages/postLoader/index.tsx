b20a26c4bd98e356a8f59f5915b0de54cec3527d
soohyun <gamjatan9@gmail.com>
2024-06-14 13:47:36 +0900

    Fix: 전시 생성 완료 페이지 수정

--- src/pages/postLoader/index.tsx
+++ src/pages/postLoader/index.tsx
@@ -1,16 +1,31 @@
-import { useParams } from 'react-router-dom';
+import { postPayment } from '@/apis/payment';
+import { useNavigate, useParams } from 'react-router-dom';
 
-// import * as S from './styles';
+import * as S from './styles';
 
 const PostLoaderPage = () => {
-  const { galleryId, cost } = useParams();
+  const navigate = useNavigate();
+  const { galleryId, cost } = useParams<{ galleryId: string; cost: string }>();
+
+  const paidGallery = async () => {
+    const payment = await postPayment(Number(galleryId), 'paidGallery');
+    window.location.href = payment.next_redirect_pc_url;
+  };
 
   return (
-    <div>
-      {cost === 'free' ? '전시 생성이 완료되었습니다.' : '결제가 필요합니다.'}
-      <br />
-      Gallery ID: {galleryId}
-    </div>
+    <S.Container>
+      {cost === 'free' ? (
+        <div>
+          전시 생성이 완료되었습니다! <br />
+          <button onClick={() => navigate('/')}>메인으로</button>
+        </div>
+      ) : (
+        <div>
+          전시 생성 중... 전시관 이용료 결제가 필요합니다! <br />
+          <button onClick={paidGallery}>결제하기</button>
+        </div>
+      )}
+    </S.Container>
   );
 };
 
