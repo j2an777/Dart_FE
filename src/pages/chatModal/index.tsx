80b524acf52c120a67b318893387becd56c7f6ea
soohyun <gamjatan9@gmail.com>
2024-06-17 09:44:08 +0900

    Feat: 채팅 로그인, 비로그인 구분

---
+++ src/pages/chatModal/index.tsx
@@ -0,0 +1,87 @@
+import { useState } from 'react';
+import { chatStore } from '@/stores/modal';
+
+import Text from '@/components/Text';
+import ChatMenu from './components/chat/ChatMenu';
+import ViewerMenu from './components/viewer/ViewerMenu';
+import * as S from './styles';
+import { memberStore } from '@/stores/member';
+import useCustomNavigate from '@/hooks/useCustomNavigate';
+
+interface Props {
+  open: boolean;
+}
+
+const ChatModal = ({ open }: Props) => {
+  const close = chatStore((state) => state.close);
+  const navigate = useCustomNavigate();
+  const isLogin = !!memberStore((state) => state.accessToken);
+  const [menuDown, setMenuDown] = useState(false);
+  const [menuOption, setMenuOption] = useState('실시간 채팅');
+  const dropDownMenu = () => {
+    setMenuDown(!menuDown);
+  };
+
+  const handleSelect = (option: string) => {
+    setMenuOption(option);
+    setMenuDown(false);
+  };
+
+  const renderContent = () => {
+    switch (menuOption) {
+      case '실시간 접속자':
+        return <ViewerMenu />;
+      case '실시간 채팅':
+        return <ChatMenu />;
+      default:
+        return <ChatMenu />;
+    }
+  };
+
+  const handleLoginClick = () => {
+    close();
+    navigate('/login');
+  };
+
+  if (!open) return null;
+
+  return (
+    <S.Container>
+      <S.Outline>
+        <S.HeaderBox>
+          <div>
+            <Text typography="t5" bold="regular">
+              {menuOption}
+            </Text>
+            <S.Select value="select" isActive={menuDown} onClick={dropDownMenu} />
+          </div>
+          <S.Close value="cancel" size={15} onClick={close} />
+        </S.HeaderBox>
+        {isLogin ? (
+          <>
+            {menuDown &&
+              (menuOption === '실시간 채팅' ? (
+                <S.Menu onClick={() => handleSelect('실시간 접속자')}>
+                  실시간 접속자
+                </S.Menu>
+              ) : (
+                <S.Menu onClick={() => handleSelect('실시간 채팅')}>실시간 채팅</S.Menu>
+              ))}
+          </>
+        ) : (
+          // 비로그인 시 비활성화
+          <S.BlurBox>
+            <Text typography="t5" bold="regular">
+              로그인 후 이용해주세요.
+            </Text>
+            <S.LoginButton onClick={handleLoginClick}> 로그인</S.LoginButton>
+          </S.BlurBox>
+        )}
+
+        <S.Content>{renderContent()}</S.Content>
+      </S.Outline>
+    </S.Container>
+  );
+};
+
+export default ChatModal;
---
+++ src/pages/chatModal/styles.ts
@@ -0,0 +1,103 @@
+import styled from '@emotion/styled';
+import { keyframes } from '@emotion/react';
+import { colors } from '@/styles/colorPalette';
+import { LayoutMap } from '@/styles/layout';
+import { typographyMap } from '@/styles/typography';
+import { Icon } from '../../components';
+import { buttonSizeMap, buttonTypeMap } from '@/styles/button';
+
+export const Container = styled.div`
+  position: fixed;
+  right: 20px;
+  top: 50%;
+  transform: translateY(-50%);
+  z-index: var(--dimmed-zindex);
+  display: inline-block;
+  height: 96vh;
+  width: 28vw;
+  padding: 10px;
+  background: rgba(255, 255, 255, 0.95);
+  border-radius: 10px;
+`;
+
+export const Outline = styled.div`
+  position: relative;
+  height: 100%;
+  border-radius: 10px;
+  border: 2px solid ${colors.black};
+`;
+
+export const HeaderBox = styled.div`
+  ${LayoutMap.displayFlex}
+  justify-content: space-between;
+  padding: 20px;
+  border-bottom: 1px solid ${colors.black};
+
+  div {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+`;
+
+const rotateIcon = keyframes`
+  0% {
+    transform: scaleY(1);
+  }
+  100% {
+    transform: scaleY(-1);
+  }
+`;
+
+export const Select = styled(Icon)<{ isActive: boolean }>`
+  display: inline-block;
+  margin-top: 2px;
+  animation: ${(props) => props.isActive && rotateIcon} 0.5s forwards;
+`;
+
+export const Menu = styled.div`
+  position: absolute;
+  ${typographyMap.t5}
+  width: 100%;
+  padding: 20px;
+  border-bottom: 1px solid ${colors.black};
+  border-radius: 0px 0px 10px 10px;
+  background: #f3f3f3;
+  z-index: 1;
+  cursor: pointer;
+  &:hover {
+    background: ${colors.gray200};
+  }
+`;
+
+export const Content = styled.div`
+  height: 100%;
+`;
+
+export const Close = styled(Icon)`
+  z-index: 11;
+`;
+
+export const BlurBox = styled.div`
+  z-index: 10;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  background: rgba(150, 150, 150, 0.5);
+  backdrop-filter: blur(2px);
+`;
+
+export const LoginButton = styled.button`
+  ${buttonSizeMap.md}
+  ${buttonTypeMap.rectangleBlack}
+  border-radius: 10px;
+`;
