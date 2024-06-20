368867f5d20df33ab1fca9d58ea7601f5ba94ccc
j2an777 <ha991104@gmail.com>
2024-06-20 18:02:52 +0900

    fix: loading 수정
    react query의 isLoading으로 수정

--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -5,33 +5,23 @@ import { useParams } from 'react-router-dom';
 import { getGallery } from '@/apis/gallery';
 import { useQuery } from '@tanstack/react-query';
 import SelectTemplate from './hooks/selectTemplate';
-import { useEffect, useState } from 'react';
 
 const GalleryPage = () => {
 
   const { galleryId: galleryIdStr } = useParams<{ galleryId?: string }>();
   const galleryId = galleryIdStr ? parseInt(galleryIdStr, 10) : NaN;
   const template = "four";
-  const [loading, setLoading] = useState(true);
 
-  const { data: galleryData, error } = useQuery({
+  const { data: galleryData, error, isLoading } = useQuery({
     queryKey: ['galleryData'],
     queryFn: () => getGallery(galleryId),
   });
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 3000); // 3초 동안 로고 로더 표시
-
-    return () => clearTimeout(timer);
-  }, []);
 
   if (error) {
     return <div>Error loading gallery data</div>;
   }
 
-  if(loading) {
+  if(isLoading) {
     return <LogoLoader/>;
   }
 
