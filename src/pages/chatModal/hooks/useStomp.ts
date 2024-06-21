e6b9559efd1d6ec5e4aceeae0d28d692d7172f8d
soohyun <gamjatan9@gmail.com>
2024-06-21 15:51:08 +0900

    Fix: websocket stomp 연결 함수 수정

---
+++ src/pages/chatModal/hooks/useStomp.ts
@@ -0,0 +1,93 @@
+import { useState } from 'react';
+import SockJS from 'sockjs-client';
+import { Client, IMessage } from '@stomp/stompjs';
+import { ChatMessageRequest, ChatMessageResponse } from '@/types/chat';
+
+export const useStomp = (
+  chatRoomId: number,
+  accessToken: string,
+  callback: (msg: ChatMessageResponse) => void,
+) => {
+  const [client, setClient] = useState<Client | null>(null);
+  const stompHeaders = {
+    Authorization: `Bearer ${accessToken}`,
+  };
+
+  const connect = () => {
+    const socket = new SockJS(import.meta.env.VITE_SOCKET_URL);
+
+    const client = new Client({
+      webSocketFactory: () => socket,
+      connectHeaders: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      reconnectDelay: 5000,
+      debug: (str) => {
+        console.log('연결', str);
+      },
+      onConnect: () => {
+        console.log('Websocket 연결');
+        subscribe();
+        setClient(client);
+      },
+    });
+
+    // 에러 메세지
+    client.onStompError = (frame) => {
+      console.error('Stomp error: ' + frame.headers['message']);
+      console.error('Additional details: ' + frame.body);
+    };
+
+    client.onWebSocketError = (event) => {
+      console.error('WebSocket Error:', event);
+    };
+
+    client.onWebSocketClose = (event) => {
+      console.error('WebSocket Closed:', event);
+    };
+
+    client.activate();
+  };
+
+  // callback 함수 수정
+  const subscribe = () => {
+    client?.subscribe(
+      `/sub/ws/${chatRoomId}`,
+      (message: IMessage) => {
+        if (message.body) {
+          try {
+            const messageData: ChatMessageResponse = JSON.parse(message.body);
+            callback(messageData);
+          } catch (error) {
+            console.error('Error:', error);
+          }
+        }
+      },
+      stompHeaders,
+    );
+    console.log('socket 구독');
+  };
+
+  const disconnect = () => {
+    client?.deactivate();
+    setClient(null);
+    console.log('WebSocket 연결 종료');
+  };
+
+  const sendMessage = (destination: string, content: ChatMessageRequest) => {
+    if (client && client.connected) {
+      client.publish({
+        destination,
+        headers: stompHeaders,
+        body: JSON.stringify(content),
+      });
+      console.log('메세지 전송');
+    } else {
+      console.error('WebSocket 연결 애러');
+    }
+  };
+
+  return { connect, disconnect, sendMessage };
+};
+
+export default useStomp;
