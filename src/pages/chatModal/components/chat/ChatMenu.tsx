dfea14fdb931f6fede940037a1a2817e7fd2332e
soohyun <gamjatan9@gmail.com>
2024-06-21 15:52:01 +0900

    Fix: 채팅 모달창 수정

--- src/pages/chatModal/components/chat/ChatMenu.tsx
+++ src/pages/chatModal/components/chat/ChatMenu.tsx
@@ -1,93 +1,100 @@
-import { useState, useEffect, useRef, useCallback } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Icon from '@/components/icon';
 import Message from './Message';
-import { ChatMessageRequest, ChatMessageResponse } from '@/types/chat';
+import { ChatMessageResponse } from '@/types/chat';
 import { useGetMessages } from '../../hooks/useGetMessages';
 import { memberStore } from '@/stores/member';
 import * as S from './styles';
-import useWebSocket from '../../hooks/useWebSocket';
+import useStomp from '../../hooks/useStomp';
 
 const ChatMenu = ({ chatRoomId }: { chatRoomId: number }) => {
-  const { nickname } = memberStore((state) => state.auth);
   const [messages, setMessages] = useState<ChatMessageResponse[]>([]);
   const [newMessage, setNewMessage] = useState<string>('');
+  const { accessToken } = memberStore.getState();
+  const { connect, disconnect, sendMessage } = useStomp(
+    chatRoomId,
+    accessToken as string,
+    (message: ChatMessageResponse) => {
+      const messageData = JSON.parse(message.content);
+      setMessages((prevMessages) => [...prevMessages, messageData]);
+    },
+  );
 
-  // 스크롤 아래로 이동
-  const ref = useRef<HTMLDivElement>(null);
-  const scrollToBottom = () => {
-    if (ref.current) {
-      ref.current.scrollTop = ref.current.scrollHeight;
-    }
-  };
-
-  // 과거 채팅
-  const { data } = useGetMessages(chatRoomId);
-  useEffect(() => {
-    if (data) {
-      setMessages(data);
-    }
-    scrollToBottom();
-  }, [data]);
-
-  // // 새로운 메시지 수신 처리
-  const handleIncomingMessage = useCallback((msg: ChatMessageResponse) => {
-    setMessages((prevMessages) => [...prevMessages, msg]);
-  }, []);
-
-  // WebSocket 연결
-  const stompClient = useWebSocket(chatRoomId, handleIncomingMessage);
+  const { data, fetchNextPage, hasNextPage } = useGetMessages(chatRoomId);
+  const observerRef = useRef<HTMLDivElement>(null);
+  const scrollRef = useRef<HTMLDivElement>(null);
 
   // 새로운 채팅 보내기
-  const sendMessage = () => {
-    if (stompClient && newMessage) {
-      const chatMessage: ChatMessageRequest = {
-        content: newMessage,
-      };
-      stompClient.publish({
-        destination: `/pub/ws/${chatRoomId}/chat-messages`,
-        body: JSON.stringify(chatMessage),
-      });
-      setMessages((prevMessages) => [
-        ...prevMessages,
-        {
-          sender: nickname,
-          content: newMessage,
-          createAt: new Date(),
-        },
-      ]);
-
-      setNewMessage('');
-    }
+  const postMessage = () => {
+    const messageBody = { content: newMessage };
+    sendMessage(`/pub/ws/${chatRoomId}/chat-messages`, messageBody);
+    setNewMessage('');
   };
 
-  // 메세지 추가될 때
+  // 웹소켓 연결 및 과거 채팅 불러오기
   useEffect(() => {
-    scrollToBottom();
+    connect();
+
+    if (data) {
+      const allMessages = data.pages.flatMap((page) => page.pages);
+      setMessages(allMessages);
+    }
+
+    return () => disconnect();
+  }, [data]);
+
+  // IntersectionObserver
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && hasNextPage) {
+          fetchNextPage();
+        }
+      },
+      { root: scrollRef.current, rootMargin: '10px', threshold: 0.5 },
+    );
+    if (observerRef.current) observer.observe(observerRef.current);
+  }, [hasNextPage, fetchNextPage]);
+
+  // 스크롤 아래로 이동
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+    }
   }, [messages]);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNewMessage(e.target.value);
+  };
+  const onEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      postMessage();
+    }
   };
 
   return (
     <S.Container>
-      <S.ContentBox ref={ref}>
-        {messages.map((msg, index) => (
-          <Message
-            key={index}
-            name={'msg.nickname'}
-            profileImage={'default'}
-            content={msg.content}
-          />
-        ))}
+      <S.ContentBox ref={scrollRef}>
+        <div ref={observerRef}>
+          {messages.map((msg, index) => (
+            <Message
+              key={index}
+              name={'msg.nickname'}
+              profileImage={'default'}
+              content={msg.content}
+            />
+            // 작가 메세지 구분 추후 추가
+          ))}
+        </div>
       </S.ContentBox>
       <S.InputBox>
         <S.Input
           placeholder="채팅 입력"
           value={newMessage}
-          onChange={handleInputChange}
+          onChange={onInputChange}
+          onKeyDown={onEnter}
         />
-        <Icon value="send" onClick={sendMessage} />
+        <Icon value="send" onClick={postMessage} />
       </S.InputBox>
     </S.Container>
   );
