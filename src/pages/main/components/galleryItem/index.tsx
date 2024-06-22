a76c3aecccc1a41e7934bf67b0ec97e8586474f2
j2an777 <ha991104@gmail.com>
2024-06-23 01:09:59 +0900

    fix: util 함수 생성 및 메인 페이지 수정
    글자 수 제한 함수 생성 및 메인 페이지 적용

--- src/pages/main/components/galleryItem/index.tsx
+++ src/pages/main/components/galleryItem/index.tsx
@@ -4,6 +4,7 @@ import parseDate from '@/utils/parseDate';
 import { galleryInfoStore } from '@/stores/modal';
 
 import * as S from './styles';
+import { TruncateSentece } from '@/utils/truncateSentence';
 
 const GalleryItem = ({ title, startDate, endDate, thumbnail, galleryId }: Gallery) => {
   const open = galleryInfoStore((state) => state.open);
@@ -12,18 +13,18 @@ const GalleryItem = ({ title, startDate, endDate, thumbnail, galleryId }: Galler
   const parsedEndDate = endDate ? new Date(endDate as Date) : null;
   const hasEnded = parsedEndDate ? parsedEndDate < today : false;
 
+  const newTitle = TruncateSentece(title, 15);
+
   return (
     <S.Container onClick={() => open(galleryId, hasEnded)}>
-      <S.ThumbnailBox>
-        <S.Thumbnail src={thumbnail} alt="thumbnail-image" />
-        {hasEnded && <S.Overlay />}
-      </S.ThumbnailBox>
+      <S.Thumbnail src={thumbnail} alt="thumbnail-image" />
       <Text color="gray500" typography="t7" bold="thin">
         {parseDate(startDate)} ~ {endDate && parseDate(endDate as Date)}
       </Text>
       <Text typography="t6" bold="regular">
-        {title}
+        {newTitle}
       </Text>
+      {hasEnded && <S.Overlay />}
     </S.Container>
   );
 };
