8978b38adc5e020277ad92ca86e11f910e818536
j2an777 <ha991104@gmail.com>
2024-06-26 14:14:14 +0900

    fix: GalleryInfoPortal 위치 변경

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,6 @@ import globalStyles from './styles/globalStyles.ts';
 import {
   AlertPortal,
   ChatPortal,
-  GalleryInfoPortal,
   NotificationPortal,
 } from './components';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -20,7 +19,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Global styles={globalStyles} />
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <GalleryInfoPortal />
         <ChatPortal />
         <AlertPortal />
         <NotificationPortal />
