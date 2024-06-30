4ee0f683c0998fbeef64af8f817537f27619625f
j2an777 <ha991104@gmail.com>
2024-06-30 16:24:01 +0900

    feat: 생성에 대한 주소 설정 추가
    오프라인 전시도 병행 주최시 위치 홍보를 위해 기능 추가

---
+++ src/types/address.ts
@@ -0,0 +1,14 @@
+export interface AddressData {
+    address : string;
+    addressType : string;
+    bname : string;
+    buildingName : string;
+    sido : string;
+    sigungu : string;
+    zonecode : string;
+}
+
+export interface AddressObj {
+    zoneCode : string;
+    areaAddress : string;
+}
