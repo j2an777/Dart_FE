3709f863277801de697e28a488d0a35e42eea9da
j2an777 <ha991104@gmail.com>
2024-07-01 17:53:44 +0900

    style: 회원정보수정 반응형 UI

--- src/pages/editMemberInfo/components/editMemberForm/styles.ts
+++ src/pages/editMemberInfo/components/editMemberForm/styles.ts
@@ -1,7 +1,9 @@
+import { Text } from '@/components';
 import { buttonSizeMap, buttonTypeMap } from '@/styles/button';
 import { colors } from '@/styles/colorPalette';
 import { LayoutMap } from '@/styles/layout';
 import { bolderMap, typographyMap } from '@/styles/typography';
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 export const Container = styled.form`
@@ -10,6 +12,11 @@ export const Container = styled.form`
   ${LayoutMap.displayFlex};
   flex-direction: column;
   gap: 30px;
+
+  @media (max-width: 1024px) {
+    width : 90vw;
+    height : auto;
+  }
 `;
 
 export const ProfileBlock = styled.div`
@@ -17,6 +24,16 @@ export const ProfileBlock = styled.div`
   height: 200px;
   ${LayoutMap.displayFlex};
   margin-top: 20px;
+
+  @media (max-width: 1024px) {
+    justify-content: space-evenly;
+  }
+
+  @media (max-width: 500px) {
+    flex-direction: column;
+    gap : 20px;
+    margin-top : 20px;
+  }
 `;
 
 export const ProfileLeft = styled.div`
@@ -26,6 +43,10 @@ export const ProfileLeft = styled.div`
   ${LayoutMap.displayFlex};
   justify-content: center;
   gap: 10px;
+
+  @media (max-width: 1024px) {
+    width : auto;
+  }
 `;
 
 export const ProfilePlus = styled.label`
@@ -45,6 +66,17 @@ export const ProfilePlus = styled.label`
   &:hover {
     cursor: pointer;
   }
+
+  @media (max-width: 1024px) {
+    right : -30px;
+    width : 25px;
+    height : 25px;
+  }
+  
+  @media (max-width: 500px) {
+    top : 0;
+    right : -35px;
+  }
 `;
 
 export const ProfilePlusBtn = styled.input`
@@ -58,6 +90,28 @@ export const ProfileRight = styled.div`
   grid-template-columns: auto 1fr;
   gap: 30px;
   align-items: center;
+
+  @media (max-width: 1024px) {
+    width : auto;
+  }
+`;
+
+interface ProfileCategoryProps {
+  type: string;
+}
+
+export const ProfileCategory = styled(Text)<ProfileCategoryProps>`
+  ${typographyMap.t5};
+
+  ${props => props.type === 'category' ? css`
+    ${bolderMap.regular};
+  ` : css`
+    ${bolderMap.medium};
+  `};
+
+  @media (max-width: 1024px) {
+    ${typographyMap.t7};
+  }
 `;
 
 export const EditBlock = styled.div`
@@ -77,6 +131,18 @@ export const NicknameBox = styled.div`
   display: grid;
   grid-template-columns: 100px 1fr 120px;
   gap: 20px;
+
+  @media (max-width : 500px) {
+    grid-template-columns: auto auto;
+    grid-template-rows: auto auto;
+  }
+`;
+
+export const NickNameText = styled(Text)`
+    @media (max-width : 500px) {
+      grid-column: 1 / span 2;
+      ${typographyMap.t6};
+    }
 `;
 
 export const IntroduceBox = styled.div`
@@ -86,9 +152,22 @@ export const IntroduceBox = styled.div`
   grid-template-columns: 100px 1fr 120px;
   grid-template-rows: 50px 50px;
   gap: 20px;
+
+  @media (max-width : 500px) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export const IntroduceText = styled(Text)`
+    @media (max-width : 500px) {
+      ${typographyMap.t6};
+    }
 `;
 
 export const Input = styled.input`
+  width: 100%;
+  box-sizing: border-box;
   padding: 10px;
   border: 1px solid #ccc;
 `;
@@ -110,6 +189,10 @@ export const CancelBtn = styled.button`
   &:hover {
     ${buttonTypeMap.rectangleGray};
   }
+
+  @media (max-width: 500px) {
+    width : 100%;
+  }
 `;
 
 export const StoreBtn = styled.button`
@@ -120,31 +203,49 @@ export const StoreBtn = styled.button`
   &:hover {
     ${buttonTypeMap.rectangleBlack};
   }
+
+  @media (max-width: 500px) {
+    width : 100%;
+  }
 `;
 
 export const ButtonContainer = styled.div`
   display: flex;
   justify-content: flex-end;
   gap: 15px;
-  grid-column: 1 / span 2;
-  grid-row: 5;
+
+  @media (max-width: 500px) {
+    width : 100%;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+  }
 `;
 
 export const Error = styled.p<{ nicknameError: string }>`
   position: absolute;
-  left: 20%;
+  left: 120px;
   bottom: -20px;
   ${typographyMap.t7};
   color: ${(props) =>
     props.nicknameError === '사용 가능한 닉네임입니다.' ? colors.green : colors.red};
+
+  @media (max-width: 500px) {
+    left : 0;
+  }
 `;
 
 export const CheckBtn = styled.button`
   width: 100%;
   height: 40px;
+  padding: 0 10px;
   ${buttonTypeMap.rectangleWhite};
 
   &:hover {
     ${buttonTypeMap.rectangleBlack};
+  }
+
+  @media (max-width: 500px) {
+    ${typographyMap.t8};
   }
 `;
