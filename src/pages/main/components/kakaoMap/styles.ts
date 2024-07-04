f61627a1bee7232fc16d7538142734f0968aaaff
j2an777 <ha991104@gmail.com>
2024-07-05 00:40:44 +0900

    fix: 카카오맵 로딩에 의한 로더 추가

--- src/pages/main/components/kakaoMap/styles.ts
+++ src/pages/main/components/kakaoMap/styles.ts
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { motion } from 'framer-motion';
 
 export const Container = styled.div`
     position : absolute;
@@ -17,4 +18,12 @@ export const Container = styled.div`
     @media (max-width: 660px) {
         display : none;
     }
+`;
+
+export const CircleLoader = styled(motion.span)`
+    width: 5rem;
+    height: 5rem;
+    border: 0.5rem solid #e9e9e9;
+    border-top: 0.5rem solid #3498db;
+    border-radius: 50%;
 `;
