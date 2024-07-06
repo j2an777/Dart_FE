50944d1d38eb8f06b3faed5d15767ee723e85c1f
j2an777 <ha991104@gmail.com>
2024-07-06 16:27:47 +0900

    style: contact 페이지 hover 내용 추가

--- src/pages/contact/styles.ts
+++ src/pages/contact/styles.ts
@@ -3,6 +3,7 @@ import { LayoutMap } from '@/styles/layout';
 import { typographyMap } from '@/styles/typography';
 import styled from '@emotion/styled';
 import Logo from '@/assets/images/luckySeven.png';
+import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
     width: 100vw;
@@ -71,10 +72,12 @@ export const BottomBox = styled.div`
 `;
 
 export const Member = styled.div`
+    position: relative;
     display: flex;
     flex-direction: column;
     margin-bottom: 40px;
     gap : 15px;
+    overflow: hidden;
 `;
 
 export const MemberImage = styled.img`
@@ -101,6 +104,56 @@ export const MemberInfo = styled.div`
         background-color: black;
     }
 `;
+
+export const LinkBox = styled(Link)`
+    position: relative;
+`;
+
+export const ContentBox = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.6);
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    justify-content: center;
+    align-items: center;
+    padding: 10px;
+    box-sizing: border-box;
+    opacity: 0;
+    transition: 0.6s;
+    color: white;
+    cursor: pointer;
+    border-radius: 20px;
+
+    * {
+      transform: translateY(25px);
+      transition: transform 0.6s;
+    }
+
+    &:hover {
+        opacity: 1;
+
+        * {
+          transform: translateY(0px);
+        }
+      }
+    }
+
+    .p {
+      word-break: break-word;
+    }
+
+    .description {
+      white-space: normal;
+      word-break: break-word;
+      text-align: center;
+    }
+`;
+
 export const MemberDescription = styled(Text)`
     margin-top : 20px;
 `;
