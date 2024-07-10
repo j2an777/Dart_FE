ca719c362e639880958a47f99beaf424072c7348
j2an777 <ha991104@gmail.com>
2024-07-10 15:27:19 +0900

    fix: contact 반응형 수정 및 이미지 변경

--- src/consts/teamUser.ts
+++ src/consts/teamUser.ts
@@ -1,3 +1,8 @@
+import changmin from '@/assets/images/changmin.png';
+import seungjin from '@/assets/images/seungjin.png';
+import sinbeom from '@/assets/images/sinbeom.png';
+import solmoon from '@/assets/images/solmoon.png';
+
 export const TeamUserData = [
     {
         id: 1,
@@ -5,7 +10,7 @@ export const TeamUserData = [
         team: 'LuckySeven',
         role: 'FE Leader',
         description: '많은 것들을 시도하여 더 나은 개발자가 되겠습니다.',
-        image: 'https://github.com/kangsinbeom.png',
+        image: sinbeom,
         github: 'https://github.com/kangsinbeom',
         mail: 'kangsinbeom@gmail.com',
     },
@@ -15,7 +20,7 @@ export const TeamUserData = [
         team: 'LuckySeven',
         role: 'BE Leader',
         description: '함께하는 팀원들에게 도움이 되는 일원이 되겠습니다.',
-        image: 'https://github.com/ChangMinPark2.png',
+        image: changmin,
         github: 'https://github.com/ChangMinPark2',
         mail: '98qkrckdals@naver.com',
     },
@@ -25,7 +30,7 @@ export const TeamUserData = [
         team: 'LuckySeven',
         role: 'FE',
         description: '사용자 경험의 발전에 이바지 하고싶은 개발자입니다.',
-        image: 'https://github.com/j2an777.png',
+        image: seungjin,
         github: 'https://github.com/j2an777',
         mail: 'ha991104@gmail.com',
     },
@@ -55,7 +60,7 @@ export const TeamUserData = [
         team: 'LuckySeven',
         role: 'BE',
         description: '한 번 시작하면 끝을 모르는 도전자입니다.',
-        image: 'https://github.com/solmoonkang.png',
+        image: solmoon,
         github: 'https://github.com/solmoonkang',
         mail: 'https://ssolv.tistory.com',
     },
